259d5cc6de146efe361402b9665f0635093367e9
alrizkipascar <alrizkipasca99@gmail.com>
2023-10-18 01:50:17 +0700

    project fix

--- src/app/projects/[slug]/page.js
+++ src/app/projects/[slug]/page.js
@@ -1,8 +1,7 @@
-"use client";
 import { kanit } from "@/app/fonts";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-import { useParams } from "next/navigation";
+// import { useParams } from "next/navigation";
 import { getProject } from "../../../../sanity/sanity-utils";
 import createBody from "@/components/Blogs/createBody";
 import Loading from "../loading";
@@ -11,53 +10,55 @@ import PhotoGrid from "@/components/Products/PhotoGrid";
 const kanit_fonts = kanit;
 // export const runtime = "edge";
 
-async function getDataProject(slug) {
+async function getDataProject(context) {
   try {
-    const blogs = await getProject(slug);
-    return blogs;
+    const slug = context.params.slug;
+    const project = await getProject(slug);
+    return project;
   } catch (error) {
     console.log("error fetching posts", error);
   }
 }
 
-export default function Project() {
-  const router = useParams();
-  const { slug } = router;
-  const [blog, setBlogs] = useState([]);
+export default async function Project(context) {
+  const project = await getDataProject(context);
+  console.log("context", project);
+  // const router = useParams();
+  // const { slug } = router;
+  // const [blog, setBlogs] = useState([]);
   //
-  useEffect(() => {
-    getDataProject(slug)
-      .then((data) => {
-        setBlogs(data);
-      })
-      .catch((error) => {
-        console.log("error fetching posts", error);
-      });
-  });
-
-  let bodyFirst = "";
-  let bodyAfter = "";
-  let blogDate = null;
-  let blogTime = null;
-  if (blog) {
-    if (blog?.parallaxImage) {
-      bodyFirst += createBody(blog?.body);
-      blogDate = new Date(blog?.publishedAt);
-    } else {
-      bodyFirst += createBody(blog?.body);
-    }
-    let newDate = new Date(blog?.publishedAt);
-    blogDate = newDate.toDateString();
-    blogTime = newDate.toLocaleTimeString();
-  }
-
-  if (blog.length == 0) {
+  // useEffect(() => {
+  //   getDataProject(slug)
+  //     .then((data) => {
+  //       setBlogs(data);
+  //     })
+  //     .catch((error) => {
+  //       console.log("error fetching posts", error);
+  //     });
+  // });
+  if (project == undefined) {
     return (
-      <div className="w-screen box-border">
+      <div className="w-full box-border">
         <Loading />{" "}
       </div>
     );
   }
+  let bodyFirst = "";
+  let bodyAfter = "";
+  let blogDate = null;
+  let blogTime = null;
+  if (project) {
+    if (project?.parallaxImage) {
+      bodyFirst += createBody(project?.body);
+      blogDate = new Date(project?.publishedAt);
+    } else {
+      bodyFirst += createBody(project?.body);
+    }
+    let newDate = new Date(project?.publishedAt);
+    blogDate = newDate.toDateString();
+    blogTime = newDate.toLocaleTimeString();
+  }
+
   return (
     <section className="bg-background w-full h-full text-textWhite">
       {/* <h1>Home Page</h1>
@@ -67,12 +68,12 @@ export default function Project() {
       <header
         className="bg-header flex items-center justify-center h-[600px] pb-12"
         style={{
-          backgroundImage: `url(${blog?.mainImage})`,
+          backgroundImage: `url(${project?.mainImage})`,
           backgroundPosition: "center",
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
         }}
-        alt={blog?.title}
+        alt={project?.title}
       >
         {/* <div className="bg-background text-textWhite font-serif mx-4 p-4 text-center md:p-8">
           <p className="text-lg">Author Name</p>
@@ -80,10 +81,10 @@ export default function Project() {
       </header>
 
       <div className="font-serif leading-normal py-12 px-4 max-w-lg">
-        <h1 className="text-5xl uppercase">{blog?.title}</h1>
+        <h1 className="text-5xl uppercase">{project?.title}</h1>
         <div className="flex gap-3">
           <p className="italic text-sm">{blogDate + " " + blogTime}</p>
-          {blog?.tags?.map((index, i) => {
+          {project?.tags?.map((index, i) => {
             return (
               <div key={i} className="flex  gap-1 items-center ">
                 <div
@@ -115,7 +116,7 @@ export default function Project() {
         ))}
       </div>
       <div className="grid grid-cols-1 pt-6 md:grid-cols-3 w-full h-full gap-3  border-t-2 border-secondary">
-        {blog?.imagesGallery?.map((index, i) => {
+        {project?.imagesGallery?.map((index, i) => {
           return <PhotoGrid data={index} key={i} />;
         })}
       </div>
